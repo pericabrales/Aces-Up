6143877847f17fd45bfd9b53cb34c4715fbda8be
Peri <cabralep@oregonstate.edu>
2019-10-26 15:05:19 -0700

    Created game as a maven project and got ninja to work.

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -18,11 +18,6 @@ public class Game {
         // initialize a new game such that each column can store cards
     }
 
-    //Main should be somewhere in here, but not sure if before or after public Game()
-    public static void main(String[] args) {
-
-    }
-
     public void buildDeck() {
         for(int i = 2; i < 15; i++){
             deck.add(new Card(i,Suit.Clubs));
